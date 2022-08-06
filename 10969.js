8df7a1c4c1cc4a42a015992b93b613801fb25114
Jaehyuksssss <ljh95310@naver.com>
2022-08-06 12:41:17 +0900

    10969 문제

---
+++ 10969.js
@@ -0,0 +1,28 @@
+// 문제
+// 두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오.
+
+// 입력
+// 두 자연수 A와 B가 주어진다. (1 ≤ A, B ≤ 10,000)
+
+// 출력
+// 첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
+
+// 예제 입력 1
+// 7 3
+
+const fs = require("fs");
+
+function main(num) {
+  const arr1 = num.split(" ");
+
+  const a = parseInt(arr1[0], 10);
+  const b = parseInt(arr1[1], 10);
+
+  console.log(a + b);
+  console.log(a - b);
+  console.log(a * b);
+  console.log(parseInt(a / b));
+  console.log(a % b);
+}
+// const inputData1 = fs.readFileSync("/dev/stdin").toString();
+main("7 3");

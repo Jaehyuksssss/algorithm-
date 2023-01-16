c8325c7b5c78751e1bb36160cd1eea5014d35133
Jaehyuksssss <ljh95310@naver.com>
2023-01-16 22:47:53 +0900

    ADD : 프로그래머스 문제

--- 프로그래머스/32.js
+++ 프로그래머스/32.js
@@ -1,6 +1,21 @@
 // 정수 배열 array가 매개변수로 주어질 때, 가장 큰 수와 그 수의 인덱스를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
 
-function solution(array) {
-  var answer = [];
-  return answer;
-}
+//(큰수 작은수 구하기)
+// function solution(array) {
+//  let resultArr =[];
+//  let answerArr = array.map((v)=>{
+//    if(v===array.reduce(function(a,b){
+//   return Math.max(a,b)
+// })){
+//      resultArr.push(v)
+//    }else if(v===array.reduce(function(a,b){
+//      return Math.min(a,b)
+//    })){
+//      resultArr.push(v)
+//    }
+//  })
+//   return resultArr.sort((a,b)=>(b-a))
+
+// }
+// console.log(solution([1, 8, 3]))
+// console.log(solution([9, 10, 11, 8]))

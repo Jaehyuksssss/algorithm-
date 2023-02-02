950ce2ea5529dabf31ce04017a60534d1d464f12
임재혁 <ljh95310@naver.com>
2023-02-02 11:03:12 +0900

    ADD: 13.js

---
+++ 프로그래머스/level-1/13.js
@@ -0,0 +1,19 @@
+// 가운데 글자 가져오기
+// 문제 설명
+// 단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+
+// 재한사항
+// s는 길이가 1 이상, 100이하인 스트링입니다.
+
+function solution(s) {
+  let arr = [s].join('').split('')
+  let mid = Math.round(arr.length / 2 - 1)
+  let answer = []
+  console.log(mid)
+  if (s.length % 2 === 1) {
+    answer.push(arr[mid])
+  } else if (s.length % 2 === 0) {
+    answer.push(arr[mid], arr[mid + 1])
+  }
+  return answer.join('')
+}

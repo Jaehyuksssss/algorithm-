86f44c16f538f931e9ac943b275d802b5155906e
임재혁 <ljh95310@naver.com>
2023-03-06 12:17:25 +0900

    ADD : 중복단어 제거

---
+++ 인프런/6.js
@@ -0,0 +1,16 @@
+//중복단어제거
+
+
+function solution(s){  
+    let answer;
+    //console.log(s.indexOf("student"));
+    answer=s.filter((v, i)=>{
+        //console.log(v, i);
+        if(s.indexOf(v)===i) return v;
+    });
+
+    
+    return answer;
+}
+let str=["good", "time", "good", "time", "student"];
+console.log(solution(str));

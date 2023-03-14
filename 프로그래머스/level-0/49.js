e03411034f09ce6af62d265c8de316d8824a9e2f
임재혁 <ljh95310@naver.com>
2023-03-14 12:57:16 +0900

    ADD : 49.js

--- 프로그래머스/level-0/49.js
+++ 프로그래머스/level-0/49.js
@@ -4,3 +4,7 @@ function solution(box, n) {
     let answer = box.map((x) => ~~(x/n)).reduce((acc, cur) => acc * cur);
     return answer;
 }
+
+~~ => 물결 연산자는 Math.floor()와 같게 쓰임
+
+소수점을 다 버리고 정수로 되어버린다.
